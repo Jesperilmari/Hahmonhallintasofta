e75fb83039720d5ecfb89e230955b0b31baa4b2f
Jesper Ojala <jesperilmariojala@gmail.com>
2025-08-19 21:55:23 +0300

    Notes-element done
    Basic notes element functionality done

--- components/notes-element.js
+++ components/notes-element.js
@@ -1,23 +1,46 @@
 import { LitElement, html, css } from 'lit';
+
 export class Notes extends LitElement {
-    static styles = css`
-    .notesBox{
-        height: 100%;
-        margin: 10px;
-        border: solid 1px lightgray;
+  static styles = css`
+    .notesBox {
+      height: 100%;
+      margin: 10px;
+      border: solid 1px lightgray;
+      padding: 5px;
+      outline: none;
+      min-height: 100px;
     }
-    .title{
-        margin-left: 10px;
-        margin-right: 10px;
-        margin-bottom: 5px;
-        font-weight: bold;
+    .title {
+      margin-left: 10px;
+      margin-right: 10px;
+      margin-bottom: 5px;
+      font-weight: bold;
+      display: block;
     }
-    `
-    render(){
-        return html`
-        <span class="title">MUISTIINPANOT</span>
-            <div contenteditable="true" class="notesBox"></div>
-        `;
+  `;
+
+  constructor() {
+    super();
+    this._key = 'notesContent';
+  }
+
+  firstUpdated() {
+    const saved = localStorage.getItem(this._key);
+    if (saved !== null) {
+      this.shadowRoot.querySelector('.notesBox').innerHTML = saved;
     }
+    const box = this.shadowRoot.querySelector('.notesBox');
+    box.addEventListener('blur', () => {
+      localStorage.setItem(this._key, box.innerHTML);
+    });
+  }
+
+  render() {
+    return html`
+      <span class="title">MUISTIINPANOT</span>
+      <div contenteditable="true" class="notesBox"></div>
+    `;
+  }
 }
+
 customElements.define('notes-element', Notes);
